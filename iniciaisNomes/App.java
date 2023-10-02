7299fbbde45a9d4f9a4207ffeb15c24876723a6d
andrefilipe1310 <139597077+andrefilipe1310@users.noreply.github.com>
2023-10-02 17:09:22 -0300

    Update App.java

--- iniciaisNomes/App.java
+++ iniciaisNomes/App.java
@@ -3,30 +3,47 @@ import java.util.Scanner;
 public class App {
 
 
-    public String  passador(String frase,int contador, String intacta, String iniciais){
-
+    public String  passador(String frase,int contador, String iniciais){
+        
+          frase = frase.trim();
           
-        if(contador == intacta.length()){
+        if(contador == frase.length()){
             
             return iniciais;
         }else {
             if (contador == 0 ){
                 iniciais = iniciais + frase.substring(0,1);
             }
-            
-            //System.out.println(frase.substring(contador, frase.length()));
+             
             
             String letra = frase.substring(contador, contador+1);
            
             if (letra.equals(" ")){
+                
                 String letraSeguinte = frase.substring(contador+1, contador+2);
-                iniciais = iniciais + letraSeguinte;
+                if (!letraSeguinte.equals(" ")) {
+
+                    // Pegando 2 ou 3 letras seguintes para saber se são "de,da,do,dos,das"
+                    String duasLetrasDepois = frase.substring(contador+1, contador+4);
+                    String tresLetrasDepois = frase.substring(contador+1, contador+5);
+                    if (!(duasLetrasDepois.equals("de ") || 
+                     duasLetrasDepois.equals("da ") ||
+                     duasLetrasDepois.equals("do ") || 
+                     tresLetrasDepois.equals("dos ") || 
+                     tresLetrasDepois.equals("das "))){
+
+                    
+                    iniciais = iniciais + letraSeguinte;
+
+                    }
+                    
+                }
+                //String duasLetrasDepois = frase.substring(contador+3, contador+4);
+                
             }
             contador++;
             
-            
-            
-            return passador(frase,contador,intacta, iniciais);
+            return passador(frase,contador, iniciais);
         }
     
     }
@@ -39,24 +56,6 @@ public class App {
         return iniciais;
     }
 
-       /*  public String iniciais(int contador,String nome){
-            contador = 0;
-            String armazenador = nome;
-            //André Filipe de Oliveira Figueiredo
-            if (contador == nome.length()){
-                return "acabou";
-            }
-            else{
-                nome = nome.substring(nome.length(),contador);
-                contador++;
-
-                System.out.println(nome);
-                
-                return iniciais(contador,nome);
-                //return iniciais(nome);
-            }
-            
-        } */
     public static void main(String[] args) {
 
         // Exemplo André Filipe de Oliveira, FIgueiredo return afof
@@ -67,9 +66,8 @@ public class App {
         String nome = "André Filipe de Oliveira Figueiredo";
 
         //app.passador(nome,0,nome);
-        System.out.println(app.passador(nome, 0, nome,""));
+        System.out.println(app.passador(nome, 0,""));
         
     }
     
     }
-
